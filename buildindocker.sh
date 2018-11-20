04305dbfb2c47aca21aee45ba2fa14bafbc6a546
Alexey Kostin <akostin@fintech.ru>
2018-11-20 13:35:11 +0300

    Build in docker

---
+++ buildindocker.sh
@@ -0,0 +1,2 @@
+docker build -t libvirtexporterbuild -f Dockerbuildimage .
+docker run --rm -v "$PWD/../../../":/go/src/ -w /go/src/github.com/rumanzo/libvirt_exporter -e GOOS=linux -e GOARCH=amd64 libvirtexporterbuild:latest go build --ldflags '-extldflags "-static"'
