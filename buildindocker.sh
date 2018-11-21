fd961d2983eb313b785f45d344a76f9393083c3c
Alexey Kostin <akostin@fintech.ru>
2018-11-21 15:30:54 +0300

    Fast docker build

--- buildindocker.sh
+++ buildindocker.sh
@@ -1,2 +1,3 @@
+rm -f libvirt_exporter
 docker build -t libvirtexporterbuild -f Dockerbuildimage .
 docker run --rm -v "$PWD/../../../":/go/src/ -w /go/src/github.com/rumanzo/libvirt_exporter -e GOOS=linux -e GOARCH=amd64 libvirtexporterbuild:latest go build --ldflags '-extldflags "-static"'
