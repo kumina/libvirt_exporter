5b2f82914293a011fb4b139d62d39d8a72b5bf3d
Bart Vercoulen <bartv@kumina.nl>
2019-02-28 11:03:15 +0100

    Fix build_static.sh

--- build_static.sh
+++ build_static.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker run -i -v `pwd`:/libvirt_exporter alpine:3.8 /bin/sh << 'EOF'
+docker run -i -v `pwd`:/gopath/src/github.com/kumina/libvirt_exporter alpine:3.8 /bin/sh << 'EOF'
 set -ex
 
 # Install prerequisites for the build process.
@@ -28,7 +28,7 @@ make install
 sed -i 's/^Libs:.*/& -lnl -ltirpc -lxml2/' /usr/local/lib/pkgconfig/libvirt.pc
 
 # Build the libvirt_exporter.
-cd /libvirt_exporter
+cd /gopath/src/github.com/kumina/libvirt_exporter
 export GOPATH=/gopath
 go get -d ./...
 go build --ldflags '-extldflags "-static"'
